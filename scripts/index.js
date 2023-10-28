d761c5f3462d2deccd69f0204fd5a7c6ea1b55f4
raphaelgraceman <raphaelgraceman@gmail.com>
2023-10-28 10:45:00 +0000

    Added hero image

--- scripts/index.js
+++ scripts/index.js
@@ -14,3 +14,38 @@ document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", 
     hamburger.classList.remove("active");
     navMenu.classList.remove("active")
 }))
+
+//Display the Current Date of the Home Region
+const daynames = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Fridays",
+    "Saturday"
+];
+//month long details
+const monthNames = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "Äugust",
+    "September",
+    "October",
+    "November",
+    "December"
+];
+const todaysdate = new Date();
+const dayName = daynames[todaysdate.getDay()];
+const monthName = monthNames[todaysdate.getMonth()];
+const year = todaysdate.getFullYear();
+document.getElementById("currentyear").textContent = year;
+const currentdate = "     " + dayName + ", " + todaysdate.getDate() + " " + monthName + ", " + todaysdate.getFullYear();
+
+document.getElementById("currentdate").textContent = currentdate;
+document.getElementById("currentDate").textContent = currentdate;
